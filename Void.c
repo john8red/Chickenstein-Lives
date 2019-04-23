4005b1725392ab18ef49cb7c0c8914e675774c80
kadajett <jeremy.ryan.stover@gmail.com>
2019-04-23 11:24:56 -0700

    almost done commit

--- Void.c
+++ Void.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
-#define SPAN ["Abuela", "Isla", "Agua", "Tortilla", "Computadora", "Compadre", "Pantalla", "Pared", "Fresa", "Teclado"]
-#define ENGL ["Grandma", "Island", "Water", "Taco", "Computer", "Partner", "Screen", "Wall", "Strawberry", "Keyboard"]
-#define RAND [4, 6, 9, 1, 7]
+// #define SPAN []
+// #define ENGL ["Grandma", "Island", "Water", "Taco", "Computer", "Partner", "Screen", "Wall", "Strawberry", "Keyboard"]
 
+const int RAND[5] = {4, 6, 9, 1, 7};
+const char SPAN[10][12] = {"Abuela", "Isla", "Agua", "Tortilla", "Computadora", "Compadre", "Pantalla", "Pared", "Fresa", "Teclado"};
+const char ENGL[10][12] = {"Grandma", "Island", "Water", "Taco", "Computer", "Partner", "Screen", "Wall", "Strawberry", "Keyboard"};
+// for something like this, its probably better to declare your functions outside of your main function
+void wordGame();
 
 int main(){
-	char y, Y;
+	char y, Y; // unused variables should probably remove. 
 	char letter;
+
+	int currentRand = 0;
 	
-	char toupper(letter);
+	// letter = toupper(letter); // letter is empty right now, yeah? Wouldn't you want to run this after it was populated? 
+	// also, if you are going to toupper it, you would want to toupper the engl answers
 	
-	void wordGame();
 	
 	printf("This is the beginning of the game");
 	printf("\nTo play this game, you must match the english word to the spanish word.");
@@ -28,21 +35,44 @@ int main(){
 	return 0;
 }
 
+
+// this is the easiest way to check. 
+// Might be better if it was a pure function.
+void guess(int currentGuess, char guessString[]) {
+	// strcmp returns 0 if all chars are the same and the length of the strings are the same
+	int isValid = strcmp(guessString, ENGL[RAND[currentGuess]]);
+	if(isValid == 0) {
+		printf("You are correct");
+	} else {
+		printf("You are wrongo doggo, the answer is: ");
+		puts(ENGL[RAND[currentGuess]]);
+	}
+}
+
 void wordGame(){
+	char currentAnswer[9];
+
 	printf("\nThis game involves the use of spanish and english words.\n");
 	printf("You must find the correct word to match to the spanish word.\n");
-	
+	int SPANLength = sizeof(SPAN);
+	for(int i = 0; i < SPANLength; i++) {
+		printf("Please translate: ");
+		puts(SPAN[RAND[i]]);
+		gets(currentAnswer);
+		guess(i, currentAnswer);
+	}
 }
+
 
 
 //don't mind this yet just trying to figure out how to do this
 //if anyone's looking at this am I comparing arrays correctly?
-char guess
+// char guess
 
-if (SPAN(RAND()) == ENGL(RAND()))
-	printf("arr[%d]", SPAN(RAND()));
-	scanf("%c", guess);
-	if (ENGL(RAND()) == guess)
-		prinf("You are correct");
-	else
-		printf("You are wrongo doggo");
+// if (SPAN(RAND()) == ENGL(RAND()))
+// 	printf("arr[%d]", SPAN(RAND()));
+// 	scanf("%c", guess);
+// 	if (ENGL(RAND()) == guess)
+// 		prinf("You are correct");
+// 	else
+// 		printf("You are wrongo doggo");
