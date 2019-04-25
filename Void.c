9eaaad28b26dbf27a6476cd9d6c32df37238dfe3
Ningen17 <49916919+Ningen17@users.noreply.github.com>
2019-04-25 11:05:14 -0500

    Update Void.c

--- Void.c
+++ Void.c
@@ -61,18 +61,12 @@ void wordGame(){
 		gets(currentAnswer);
 		guess(i, currentAnswer);
 	}
+	while(score < 6){
+		//if(currentGuess == isValid)
+			score++;
+	}
+		if(score > 5){
+		printf("You are donezo bumzo");
+		}
 }
 
-
-
-//don't mind this yet just trying to figure out how to do this
-//if anyone's looking at this am I comparing arrays correctly?
-// char guess
-
-// if (SPAN(RAND()) == ENGL(RAND()))
-// 	printf("arr[%d]", SPAN(RAND()));
-// 	scanf("%c", guess);
-// 	if (ENGL(RAND()) == guess)
-// 		prinf("You are correct");
-// 	else
-// 		printf("You are wrongo doggo");
